0ccbfa7b491b8ad3a991a111c03639babe1a1cb6
MymekDeveloper <szwed.szymon1@gmail.com>
2022-01-06 14:39:44 +0100

    Creating a constructor

--- app/src/main/java/com/example/szwedprojectportfolio/MainActivity.java
+++ app/src/main/java/com/example/szwedprojectportfolio/MainActivity.java
@@ -10,6 +10,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Project gettingStartedProject = new Project("Getting Started App", "Our very first project", 5);
+
+        Project[] projects = {
+
+                new Project("Getting Started App", "Our very first project", 5),
+                new Project("Motivation quote", "Making a simple change to the layout, with my personal motivation quote", 5),
+                new Project("BMI Calculator", "Working BMI calculator", 5),
+                new Project("Inches Convertor", "A basic converter to convert inches to meters", 5),
+                new Project("Rap&Pub", "Restaurant's application with distinguished menu", 5)
+        };
+
 
     }
 }
